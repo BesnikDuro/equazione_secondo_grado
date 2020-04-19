c060137de028f7410524df35b2f6427ef06ad5b8
Besnik Duro <nikduro@gmail.com>
2020-04-19 22:29:04 +0200

    Ri modificato listato eq_grado_2.c

--- eq_grado_2.c
+++ eq_grado_2.c
@@ -1,45 +1,56 @@
-/*risolve un'equazione di secondo grado di forma a x2 + b x + c = 0
-  controllando se esistono le soluzioni*/
+/*
+    Zgjidhja e nje ekuazione tipik te grades se dyte i cili ka formen ax2 + bx + c = 0
+    Duhet me pas parasysh dhe controlluar qe a te jete e ndryshme nga 0
+*/
 
 #include <stdio.h>
 #include <math.h>
 
-int main() {
-   float a,b,c;
-   double x1,x2,d;
+int main()
+{
+	float a, b, c, delta;
+	float x, x1, x2;
+	printf("\n");	
 
-   printf("coefficiente a:\t");
-   scanf("%f", &a);
-   printf("\ncoefficiente b:\t");
-   scanf("%f", &b);
-   printf("\ncoefficiente c:\t");
-   scanf("%f", &c);
+	printf("\tinserisci il primo coficiente a: ");
+	scanf("%f", &a);
+	printf("\tinserisci il secondo coficiente b: ");
+	scanf("%f", &b);
+	
+	if ( c != 0 ) {
+		printf("\tinserisci il terzo coficiente c: ");
+		scanf("%f", &c);
+		}	
 
-   if (a == 0) {
-	if((b == 0) && (c == 0))
-	    printf("\nequazione indeterminata");
-	else if(b == 0)
-	    printf("\nequazione impossibile");
-	else {
-	   printf("l'equazione e' di primo grado\n");
-	   x1 = -c/b;
-	   printf("\n x= %lf", x1);
-	}
-  }
-  else {
-	d= b*b - 4*a*c; 
-	printf("\nil determinante e' %5.3lf ",d);
-	if(d > 0) {
-	    x1 = (-b - sqrt(d)) / (2*a);
-	    x2 = (-b + sqrt(d)) / (2*a);
-	    printf("\nDue soluzioni reali distinte x1= %5.3lf e x2= %5.3lf ",x1,x2);
-	  }
-	  else if(d == 0) {
-	    x1 = (-b) / (2*a); 
-	    printf("\nDue soluzioni reali coincidenti x1 e x2 uguali a %5.3lf ",x1);
-	 }
-	 else printf("\nNon esistono soluzioni reali\n");
-  }
-//system("pause");
+	if( a != 0)
+		{
+		if( c == 0 )
+			{
+			c = 0;
+			//printf(" Inserisci valore 0 al cofficiente c\n");
+			(x*x + b*x) == 0;
+			x1 = 0;
+			x2 = -( b/a);
+			printf("\tSoluzioni sono 2: x1 = %f e x2 = %f\n", x1, x2);
+			}
+	
+		else
+			if(b == 0){
+				x1 = -c/a;
+				printf("\tx1 e uguale a x2 =%f\n", x1);	
+			}
+		
+			delta = (b * b - 4 * a * c);
+			if (delta <=0)
+				printf("\tdelta e' = %f \tnon ci sono radici ", delta);	
+		else
+			if (delta != 0){
+				x1 = -(b + sqrt(b * b - 4 * a * c)) / (2 * a);
+				x2 = -(b - sqrt(b * b - 4 * a * c)) / (2 * a);
+				printf("\til risultato e’ x1 = %f, x2 = %f\n", x1, x2);
+			}
+			
+		}
+
 return 0;
 }
