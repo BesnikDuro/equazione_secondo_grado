3f4aa0061278b87143ef9af0beb34a28fe09b62a
Besnik Duro <nikduro@gmail.com>
2020-04-19 22:20:45 +0200

    Modificato completo tutto il listato

--- eq_grado_2.c
+++ eq_grado_2.c
@@ -1,23 +1,45 @@
-#include <stdio.h>
-#include <math.h>
-
 /*risolve un'equazione di secondo grado di forma a x2 + b x + c = 0
   controllando se esistono le soluzioni*/
 
-int main(void)
-{
-    float a, b, c, delta;
+#include <stdio.h>
+#include <math.h>
 
-    printf("Dammi i coefficienti dell'equazione\n");
-    scanf("%f", &a);
-    scanf("%f", &b);
-    scanf("%f", &c);
-    /*calcolo il delta*/
-    delta = b*b -(4*a*c);
-    if (delta < 0 )
-        printf("l'equazione non ammette soluzioni reali\n");
-    else{
-        printf("prima soluzione %f\n", (-b + sqrt(delta)) / (2*a));
-        printf("seconda soluzione %f\n", (-b -  sqrt(delta)) / (2*a));
-    }
+int main() {
+   float a,b,c;
+   double x1,x2,d;
+
+   printf("coefficiente a:\t");
+   scanf("%f", &a);
+   printf("\ncoefficiente b:\t");
+   scanf("%f", &b);
+   printf("\ncoefficiente c:\t");
+   scanf("%f", &c);
+
+   if (a == 0) {
+	if((b == 0) && (c == 0))
+	    printf("\nequazione indeterminata");
+	else if(b == 0)
+	    printf("\nequazione impossibile");
+	else {
+	   printf("l'equazione e' di primo grado\n");
+	   x1 = -c/b;
+	   printf("\n x= %lf", x1);
+	}
+  }
+  else {
+	d= b*b - 4*a*c; 
+	printf("\nil determinante e' %5.3lf ",d);
+	if(d > 0) {
+	    x1 = (-b - sqrt(d)) / (2*a);
+	    x2 = (-b + sqrt(d)) / (2*a);
+	    printf("\nDue soluzioni reali distinte x1= %5.3lf e x2= %5.3lf ",x1,x2);
+	  }
+	  else if(d == 0) {
+	    x1 = (-b) / (2*a); 
+	    printf("\nDue soluzioni reali coincidenti x1 e x2 uguali a %5.3lf ",x1);
+	 }
+	 else printf("\nNon esistono soluzioni reali\n");
+  }
+//system("pause");
+return 0;
 }
